a04ca9b01657dccf75a800f4be16e00d1513b14c
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-07-29 21:23:57 -0400

    commit tree f91f6a8dee3cdcdc78ef01b35e4f0ac45390be52

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(leftNumbers, 10);
+    if (right >= 179) {
+      dodger.style.left = `${right + 1}px`;
+    }
+  }
+  
